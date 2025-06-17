6ce0893611ee2d945b3cd2793e8e9b8d24b9a481
HieronymusEsteban <stephane.hess@bluewin.ch>
2025-06-17 20:50:46 +0200

    Improved unit_test_sort_img_files.py.

--- source/unit_test_sort_img_files.py
+++ source/unit_test_sort_img_files.py
@@ -1,4 +1,5 @@
 import unittest
+import shutil
 from . import image_id_converter as img_id_c
 from . import sort_img_files as sif
 from pathlib import Path
@@ -21,6 +22,19 @@ class TestCompleteImageIds(unittest.TestCase):
         self.assertEqual(sif.sort_img_files(self.my_image_dir, self.my_model, 
         self.my_output_with_person_dir, self.my_output_without_person_dir), 
         self.my_expected_output)
+        # Move image files back to test data folder so the text can be executed again next time: 
+        tif_files_pers = [f for f in os.listdir(self.my_output_with_person_dir) if f.endswith('.tif')]
+        for file in tif_files_pers:
+            file_path_origin = os.path.join(self.my_output_with_person_dir, file)
+            file_path_dest = os.path.join(self.my_image_dir, file)
+            shutil.copy(file_path_origin, file_path_dest)
+
+        tif_files_without_pers = [f for f in os.listdir(self.my_output_without_person_dir) if f.endswith('.tif')]
+        for file in tif_files_without_pers:
+            file_path_origin = os.path.join(self.my_output_without_person_dir, file)
+            file_path_dest = os.path.join(self.my_image_dir, file)
+            shutil.copy(file_path_origin, file_path_dest)
+    
     
 if __name__ == "__main__":
     unittest.main()
